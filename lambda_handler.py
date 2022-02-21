062868e0e8e9e76b225212b4625888c03b8f516a
Alex Johnson <aijphy@Alexs-Air.lan>
2022-02-21 12:15:41 -0700

    dev

--- lambda_handler.py
+++ lambda_handler.py
@@ -9,5 +9,22 @@ def lambda_handler(event, context):
       transactionAmount = event['queryStringParameters']['amount']
 
       print('transactionId=' + transactionId)
-      print('transactionType='+transactionType)
-      print('transactionAmount='+transactionAmount)
+      print('transactionType=' + transactionType)
+      print('transactionAmount=' + transactionAmount)
+
+      #2. Construct the body of the response object
+      transactionResponse = {}
+      transactionResponse['transactionId'] = transactionId
+      transactionResponse['type'] = transactionType
+      transactionResponse['amount'] = transactionAmount
+      transactionResponse['message'] = 'Hello from Lambda land'
+
+      #3. Construct http response object
+      responseObject = {}
+      responseObject['statusCode'] = 200
+      responseObject['headers'] = {}
+      responseObject['headers']['Content-Type'] = 'application/json'
+      responseObject['body'] = json.dumps(transactionResponse)
+
+      #4 Return the response object
+      return responseObject
